2782a6d999b0eb61b5f02c5ba536570dfc4f512c
Kaixinwang99 <2358925w@student.gla.ac.uk>
2019-01-21 18:16:14 +0000

    end of Chaper 3

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'kux^bq=m^(kjtcz7m17_&k*mpcvb2a$*kpi8*=q+wi(jbyt&ha'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.181','127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rango',
 ]
 
 MIDDLEWARE = [
