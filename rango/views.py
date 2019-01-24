02c5d278e0f451b8ee343719add4c96be6efb1bc
Kaixinwang99 <2358925w@student.gla.ac.uk>
2019-01-24 12:12:37 +0000

    end of Chaper 4 exercise

--- rango/views.py
+++ rango/views.py
@@ -9,5 +9,6 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
     
 def about(request):
-    #<a href="/rango/">Index</a>
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context1={'boldmessage': "This tutorial has been put together by Kaixin Wang"}
+    return render(request, 'rango/about.html', context=context1)
