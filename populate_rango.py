6862bd253b3f43a14957b49219ad9accf4e71c6a
Kaixinwang99 <2358925w@student.gla.ac.uk>
2019-01-25 14:24:12 +0000

    chapter 5 exercise try

--- populate_rango.py
+++ populate_rango.py
@@ -1,6 +1,7 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'tango_with_django_project.settings')
+                      'tango_with_django_project.settings')
+
 
 import django
 django.setup()
@@ -30,12 +31,18 @@ def populate():
         {"title":"Flask",
          "url":"http://flask.pocoo.org"} ]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages,
+                       "views": p.views,
+                       "likes": p.likes},
+            "Django": {"pages": django_pages,
+                       "views": p.views,
+                       "likes": p.likes},
+            "Other Frameworks": {"pages": other_pages,
+                                 "views": p.views,
+                                 "likes": p.likes} }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, p.views, p.likes)
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -51,8 +58,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
